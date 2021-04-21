4d3a0911f6651060af2c1de5672884dccc42c272
fabicon2882 <43656659+fabicon2882@users.noreply.github.com>
2021-04-21 16:17:44 -0500

    ticket landing page added Closes #6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { DefaultLayout } from './components/layout/DefaultLayout';
+import { Ticket } from './pages/ticket/Ticket.page';
 //import { Dashboard } from './pages/dashboard/Dashboard.page';
 //import { Entry } from './pages/entry/Entry.page';
 // /import { AddTicket } from './pages/new-ticket/AddTicket.page';
-import { TicketLists } from './pages/ticket-list/TicketLists.page';
+//import { TicketLists } from './pages/ticket-list/TicketLists.page';
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
       <DefaultLayout>
         {/* <Dashboard /> */}
         {/* <AddTicket /> */}
-        <TicketLists />
+        {/* <TicketLists /> */}
+        <Ticket />
       </DefaultLayout>
     </div>
   );
